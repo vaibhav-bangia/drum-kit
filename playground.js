e3e13c38948313c40bc667068c84056e29a40c49
Vaibhav Bangia <vaibhavbangia@gmail.com>
2021-07-04 21:24:05 +0530

    completed
    added event listeners to keypress and click events

---
+++ playground.js
@@ -0,0 +1,10 @@
+function BelloBoy(name,age,workPermit,languages) {
+    this.name = name;
+    this.age = age;
+    this.workPermit = workPermit;
+    this.languages = languages;
+}
+
+var BelloBoy1 = new BelloBoy('Vicky Arora',22,false,["english","c++"])
+
+console.log(BelloBoy1)  
